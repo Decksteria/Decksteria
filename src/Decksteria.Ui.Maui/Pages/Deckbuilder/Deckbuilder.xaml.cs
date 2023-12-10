b2049c8f86dc60f7100a4ba50bf710419c7a07b9
Ere <animeftw@live.co.uk>
2023-12-10 12:55:23 +1100

    Deckbuilder Page Changes

--- src/Decksteria.Ui.Maui/Pages/Deckbuilder/Deckbuilder.xaml.cs
+++ src/Decksteria.Ui.Maui/Pages/Deckbuilder/Deckbuilder.xaml.cs
@@ -1,9 +1,12 @@
 namespace Decksteria.Ui.Maui.Pages.Deckbuilder;
 
+using Decksteria.Core;
 using Decksteria.Services.Deckbuilding;
 using Decksteria.Services.FileService.Models;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using System;
+using System.Collections.ObjectModel;
 
 public partial class Deckbuilder : ContentPage
 {
@@ -11,11 +14,37 @@ public partial class Deckbuilder : ContentPage
 
     // private readonly IDeckbuildingService deckbuilder;
 
-    public Deckbuilder()
+    public Deckbuilder(IDecksteriaFormat format)
     {
         InitializeComponent();
         BindingContext = viewModel;
         // this.deckbuilder = ;
+
+        CollectionView RenderCollectionView(IDecksteriaDeck decksteriaDeck)
+        {
+            var bindedCollection = new ObservableCollection<CardArt>();
+            viewModel.Decks.Add(decksteriaDeck, bindedCollection);
+
+            // Header Label
+            var headerLabel = new Label
+            {
+                Text = $"{decksteriaDeck.DisplayName}:",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                VerticalTextAlignment = TextAlignment.Start,
+                HorizontalTextAlignment = TextAlignment.Center,
+            };
+            headerLabel.SetDynamicResource(Label.TextColorProperty, "SecondaryTextColor");
+
+            // Collection View
+            var collectionView = new CollectionView
+            {
+                Header = headerLabel,
+                ItemTemplate = CollectionView_CardItem,
+                ItemsSource = bindedCollection
+            };
+            return collectionView;
+        }
     }
 
     private async void ContentPage_LoadedAsync(object sender, EventArgs e)
