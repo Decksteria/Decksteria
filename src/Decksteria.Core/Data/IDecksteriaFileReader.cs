f298ebba0be16dc0907af1b3deaf9ec21668938b
Ere <animeftw@live.co.uk>
2024-02-12 11:47:24 +1100

    Convert DecksteriaFileReader to Scoped

--- src/Decksteria.Core/Data/IDecksteriaFileReader.cs
+++ src/Decksteria.Core/Data/IDecksteriaFileReader.cs
@@ -13,7 +13,7 @@ public interface IDecksteriaFileReader
     /// <param name="connectionProperties">A list of all other Database Connection Properties to be included in the Connection String.</param>
     /// <param name="downloadURL">The URL which the raw file can be downloaded from if unavailable.</param>
     /// <returns>A connection string used to connect to a database includes the absolute path.</returns>
-    public string BuildConnectionString(string fileName, IDictionary<string, string> connectionProperties, string downloadURL, CancellationToken cancellationToken = default);
+    public string BuildConnectionString(string fileName, string gameName, IDictionary<string, string> connectionProperties, string downloadURL, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Used by any <see cref="IDecksteriaGame"/> Plug-Ins to get the absolute path of a specific file.
@@ -21,7 +21,7 @@ public interface IDecksteriaFileReader
     /// <param name="fileName">The file whose Absolute Path is being requested.</param>
     /// <param name="downloadURL">The URL which the raw file can be downloaded from if unavailable.</param>
     /// <returns>The absolute path of the file.</returns>
-    public Task<string> GetFileLocationAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default);
+    public Task<string> GetFileLocationAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Used by any <see cref="IDecksteriaGame"/> Plug-Ins to read local binary files.
@@ -29,7 +29,7 @@ public interface IDecksteriaFileReader
     /// <param name="fileName">The file that needs to be read. Do not include the path.</param>
     /// <param name="downloadURL">The URL which the raw file can be downloaded from if unavailable.</param>
     /// <returns>The binary contents of the file requested by the Plug-In as a <see cref="byte[]"/>.</returns>
-    public Task<byte[]> ReadByteFileAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default);
+    public Task<byte[]> ReadByteFileAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Used by any <see cref="IDecksteriaGame"/> Plug-Ins to read the image files of available cards in the Plug-In.
@@ -38,7 +38,7 @@ public interface IDecksteriaFileReader
     /// <param name="downloadURL">The URL which the raw file can be downloaded from if unavailable.</param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public Task<byte[]> ReadImageAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default);
+    public Task<byte[]> ReadImageAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Used by any <see cref="IDecksteriaGame"/> Plug-Ins to read local text files.
@@ -46,5 +46,5 @@ public interface IDecksteriaFileReader
     /// <param name="fileName">The file that needs to be read. Do not include the path.</param>
     /// <param name="downloadURL">The URL which the raw file can be downloaded from if unavailable.</param>
     /// <returns>The contents of the file requested by the Plug-In as a <see cref="string"/>.</returns>
-    public Task<string> ReadTextFileAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default);
+    public Task<string> ReadTextFileAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default);
 }
--- src/Decksteria.Ui.Maui/Services/FileReader/DecksteriaFileReader.cs
+++ src/Decksteria.Ui.Maui/Services/FileReader/DecksteriaFileReader.cs
@@ -7,25 +7,22 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Decksteria.Core.Data;
-using Decksteria.Services.PlugInFactory;
 using Microsoft.Maui.Storage;
 
-internal sealed class DecksteriaFileReader(IDecksteriaPlugInFactory plugInFactory, HttpClient httpClient) : IDecksteriaFileReader
+internal sealed class DecksteriaFileReader(IHttpClientFactory httpClientFactory) : IDecksteriaFileReader
 {
-    private readonly IDecksteriaPlugInFactory plugInFactory = plugInFactory;
-    private readonly HttpClient httpClient = httpClient;
+    private readonly HttpClient httpClient = httpClientFactory.CreateClient();
 
-    public string BuildConnectionString(string fileName, IDictionary<string, string> connectionProperties, string downloadURL, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+    public string BuildConnectionString(string fileName, string gameName, IDictionary<string, string> connectionProperties, string downloadURL, CancellationToken cancellationToken = default) => throw new NotImplementedException();
 
-    public Task<byte[]> ReadImageAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default)
+    public Task<byte[]> ReadImageAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<string> GetFileLocationAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default)
+    public async Task<string> GetFileLocationAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default)
     {
-        var plugIn = plugInFactory.GetSelectedFormat();
-        var filePath = @$"{FileSystem.AppDataDirectory}\{plugIn.Game}\{fileName}";
+        var filePath = @$"{FileSystem.AppDataDirectory}\{gameName}\{fileName}";
         if (!File.Exists(filePath))
         {
             using var httpStream = await httpClient.GetStreamAsync(downloadURL);
@@ -36,11 +33,11 @@ internal sealed class DecksteriaFileReader(IDecksteriaPlugInFactory plugInFactor
         return filePath;
     }
 
-    public Task<byte[]> ReadByteFileAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+    public Task<byte[]> ReadByteFileAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default) => throw new NotImplementedException();
 
-    public async Task<string> ReadTextFileAsync(string fileName, string downloadURL, CancellationToken cancellationToken = default)
+    public async Task<string> ReadTextFileAsync(string fileName, string gameName, string downloadURL, CancellationToken cancellationToken = default)
     {
-        var fileLocation = await GetFileLocationAsync(fileName, downloadURL, cancellationToken);
+        var fileLocation = await GetFileLocationAsync(fileName, gameName, downloadURL, cancellationToken);
         return await File.ReadAllTextAsync(fileLocation, cancellationToken);
     }
 }
