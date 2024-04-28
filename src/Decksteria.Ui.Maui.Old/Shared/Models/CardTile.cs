161c2f873196f740e58fc091900ca6ac086e4cff
Ere <animeftw@live.co.uk>
2024-04-28 15:36:25 +1000

    Delete oldproject

--- src/Decksteria.Ui.Maui.Old/Shared/Models/CardTile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Decksteria.Ui.Maui.Shared.Models;
-
-using Decksteria.Services.FileService.Models;
-
-internal record CardTile(long CardId, long ArtId, string ImageUrl, string FileName, string Details) : CardArt(CardId, ArtId, ImageUrl, FileName, Details)
-{
-    public string LocalFileLocation => FileName;
-}
