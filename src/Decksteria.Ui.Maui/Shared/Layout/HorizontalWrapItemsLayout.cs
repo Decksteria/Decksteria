4643e76b531e81e98993b710a12ea1d231e41924
Ere <animeftw@live.co.uk>
2024-05-03 19:23:16 +1000

    things

---
+++ src/Decksteria.Ui.Maui/Shared/Layout/HorizontalWrapItemsLayout.cs
@@ -0,0 +1,10 @@
+﻿namespace Decksteria.Ui.Maui.Shared.Layout;
+
+using Microsoft.Maui.Controls;
+
+internal class HorizontalWrapItemsLayout : ItemsLayout
+{
+    public HorizontalWrapItemsLayout() : base(ItemsLayoutOrientation.Horizontal)
+    {
+    }
+}
---
+++ src/Decksteria.Ui.Maui/Shared/Layout/SwitchingGridLayout.cs
@@ -0,0 +1,12 @@
+﻿namespace Decksteria.Ui.Maui.Shared.Layout;
+
+using Microsoft.Maui.Layouts;
+using UraniumUI.Layouts;
+
+internal sealed class SwitchingGridLayout : GridLayout
+{
+    protected override ILayoutManager CreateLayoutManager()
+    {
+        return new SwitchingGridLayoutManager(this);
+    }
+}
