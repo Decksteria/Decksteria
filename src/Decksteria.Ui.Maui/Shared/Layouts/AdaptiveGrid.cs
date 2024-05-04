b0f55f726d400e7ce0a34863930705132fbdf87f
Ere <animeftw@live.co.uk>
2024-05-04 14:58:50 +1000

    Add Nearly working

--- src/Decksteria.Ui.Maui/Shared/Layouts/AdaptiveGrid.cs
+++ src/Decksteria.Ui.Maui/Shared/Layouts/AdaptiveGrid.cs
@@ -3,10 +3,15 @@
 using System.ComponentModel;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Layouts;
 
 internal sealed class AdaptiveGrid : Grid
 {
-    public int RowCount { get => (int) GetValue(RowCountProperty); set => SetValue(RowCountProperty, value); }
+    public int RowCount
+    {
+        get => (int) GetValue(RowCountProperty);
+        set => SetValue(RowCountProperty, value);
+    }
 
     public static readonly BindableProperty RowCountProperty = BindableProperty.Create(
         nameof(RowCount), typeof(int), typeof(AdaptiveGrid),
@@ -15,11 +20,15 @@ internal sealed class AdaptiveGrid : Grid
         {
             if (bindable is AdaptiveGrid grid)
             {
-                grid.SetRowDefinitions(!IsLandscape(grid.Width, grid.Height));
+                grid.SetRowDefinitions(!IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
             }
         });
 
-    public int ColumnCount { get => (int) GetValue(ColumnCountProperty); set => SetValue(ColumnCountProperty, value); }
+    public int ColumnCount
+    {
+        get => (int) GetValue(ColumnCountProperty);
+        set => SetValue(ColumnCountProperty, value);
+    }
 
     public static readonly BindableProperty ColumnCountProperty = BindableProperty.Create(
         nameof(ColumnCount), typeof(int), typeof(AdaptiveGrid),
@@ -28,13 +37,55 @@ internal sealed class AdaptiveGrid : Grid
         {
             if (bindable is AdaptiveGrid grid)
             {
-                grid.SetColumnDefinitions(IsLandscape(grid.Width, grid.Height));
+                grid.SetColumnDefinitions(IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
             }
         });
 
+    /// <summary>
+    /// If the orientation is horizontal by default, it will switch to a vertical Grid orientation if the width is smaller than this property.
+    /// If the orientation is vertical by default, it will switch to a horizontal Grid orientation if the height is smaller than this property.
+    /// </summary>
+    public double DimensionSwitchSize
+    {
+        get => (double) GetValue(DimensionSwitchSizeProperty);
+        set => SetValue(DimensionSwitchSizeProperty, value);
+    }
+
+    /// <summary>Bindable property for <see cref="DimensionSwitchSize"/>.</summary>
+    public static readonly BindableProperty DimensionSwitchSizeProperty = BindableProperty.Create(nameof(DimensionSwitchSize), typeof(double),
+        typeof(Grid), 500d, propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if (bindable is AdaptiveGrid grid)
+            {
+                grid.SetColumnDefinitions(IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
+                grid.SetRowDefinitions(!IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
+            }
+        });
+
+    public StackOrientation Orientation
+    {
+        get => (StackOrientation) GetValue(OrientationProperty);
+        set => SetValue(OrientationProperty, value);
+    }
+
+    /// <summary>Bindable property for <see cref="Orientation"/>.</summary>
+    public static readonly BindableProperty OrientationProperty = BindableProperty.Create(nameof(Orientation), typeof(StackOrientation),
+        typeof(StackLayout), StackOrientation.Horizontal, propertyChanged:  (bindable, oldValue, newValue) =>
+        {
+            if (bindable is AdaptiveGrid grid)
+            {
+                grid.InvalidateMeasure();
+            }
+        });
+
+    protected override ILayoutManager CreateLayoutManager()
+    {
+        return base.CreateLayoutManager();
+    }
+
     protected override void OnSizeAllocated(double width, double height)
     {
-        var isLandscape = IsLandscape(width, height);
+        var isLandscape = IsLandscape(width, height, DimensionSwitchSize, Orientation);
         SetColumnDefinitions(!isLandscape);
         SetRowDefinitions(isLandscape);
         base.OnSizeAllocated(width, height);
@@ -77,7 +128,7 @@ internal sealed class AdaptiveGrid : Grid
         {
             if (bindable is AdaptiveGrid grid)
             {
-                grid.SetColumnDefinitions(!IsLandscape(grid.Width, grid.Height));
+                grid.SetColumnDefinitions(!IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
             }
         });
 
@@ -90,12 +141,16 @@ internal sealed class AdaptiveGrid : Grid
         {
             if (bindable is AdaptiveGrid grid)
             {
-                grid.SetRowDefinitions(IsLandscape(grid.Width, grid.Height));
+                grid.SetRowDefinitions(IsLandscape(grid.Width, grid.Height, grid.DimensionSwitchSize, grid.Orientation));
             }
         });
 
-    private static bool IsLandscape(double width, double height)
+    private static bool IsLandscape(double width, double height, double switchSize, StackOrientation stackOrientation)
     {
-        return width >= height;
+        return stackOrientation switch
+        {
+            StackOrientation.Vertical => height < switchSize,
+            _ => width > switchSize
+        };
     }
 }
