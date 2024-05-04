0d48fe3f6f2452d3cbb56ed2948f9e27e108e78e
Ere <animeftw@live.co.uk>
2024-05-04 17:08:03 +1000

    Add images

---
+++ src/Decksteria.Ui.Maui/Shared/Layouts/DataGridCollectionView.cs
@@ -0,0 +1,12 @@
+﻿namespace Decksteria.Ui.Maui.Shared.Layouts;
+
+using Microsoft.Maui;
+using UraniumUI.Layouts;
+
+internal class DataGridCollectionView : GridLayout
+{
+    protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+    {
+        return base.OnMeasure(widthConstraint, heightConstraint);
+    }
+}
