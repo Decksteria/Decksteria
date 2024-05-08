b7bbea516117fffbd78c72dd616296afb6fbc3e0
Ere <animeftw@live.co.uk>
2024-05-08 22:30:45 +1000

    Modal changes

--- src/Decksteria.Ui.Maui/Pages/Search/SearchModal.xaml.cs
+++ src/Decksteria.Ui.Maui/Pages/Search/SearchModal.xaml.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using Decksteria.Core.Models;
 using Decksteria.Services.PlugInFactory.Models;
+using Decksteria.Ui.Maui.Services.PageService;
 using UraniumUI.Pages;
 
 public partial class SearchModal : UraniumContentPage
 {
-    private readonly SearchModalViewModel viewModel;
+    private readonly SearchModalViewModel viewModel = new();
 
-    public SearchModal(GameFormat gameFormat)
+    private readonly IPageService pageService;
+
+    public SearchModal(GameFormat gameFormat, IPageService pageService)
     {
         InitializeComponent();
         viewModel.SearchFieldFilters = gameFormat.Format.SearchFields.Select(s => new SearchFieldFilter(s, ComparisonType.Equals));
+        this.pageService = pageService;
     }
 
-    public int? ValueTest { get; set; }
+    private async void Button_Pressed(object sender, System.EventArgs e)
+    {
+        await pageService.PopModalAsync<SearchModal>();
+    }
 }
