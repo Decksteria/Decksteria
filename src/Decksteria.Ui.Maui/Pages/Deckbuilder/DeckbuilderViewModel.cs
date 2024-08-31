059578312da83e5f824dece444684e4c073b1658
Ere <animeftw@live.co.uk>
2024-08-31 13:37:36 +1000

    Create menu

--- src/Decksteria.Ui.Maui/Pages/Deckbuilder/DeckbuilderViewModel.cs
+++ src/Decksteria.Ui.Maui/Pages/Deckbuilder/DeckbuilderViewModel.cs
@@ -3,24 +3,33 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using Decksteria.Services.FileService.Models;
+using UraniumUI.Icons.FontAwesome;
 using UraniumUI.Material.Controls;
 
 internal class DeckbuilderViewModel : INotifyPropertyChanged
 {
+    internal enum SortDeckStatus
+    {
+        Unsorted,
+        Ascending,
+        Descending
+    }
+
     public string ActiveDeckTab { get; set; } = string.Empty;
 
     private TabViewTabPlacement _tabPlacement = TabViewTabPlacement.Top;
 
     private bool _searching = false;
 
+    private SortDeckStatus _sortStatus = SortDeckStatus.Unsorted;
+
     private string _windowTitle = string.Empty;
 
     public Dictionary<string, ObservableCollection<CardArt>> Decks { get; set; } = [];
 
     public ObservableCollection<CardArt> FilteredCards { get; set; } = [];
-
-    public event PropertyChangedEventHandler? PropertyChanged;
 
     public bool Searching
     {
@@ -31,6 +40,25 @@ internal class DeckbuilderViewModel : INotifyPropertyChanged
             OnPropertyChanged(nameof(Searching));
         }
     }
+
+    public SortDeckStatus SortStatus
+    {
+        get => _sortStatus;
+        set
+        {
+            _sortStatus = value;
+            OnPropertyChanged(nameof(SortStatus));
+            OnPropertyChanged(nameof(SortDeckIcon));
+        }
+    }
+
+    public string SortDeckIcon => SortStatus switch
+    {
+        SortDeckStatus.Unsorted => Solid.Sort,
+        SortDeckStatus.Ascending => Solid.SortDown,
+        SortDeckStatus.Descending => Solid.SortUp,
+        _ => throw new UnreachableException($"An option for {nameof(SortStatus)} is unavailable.")
+    };
 
     public string SearchText { get; set; } = "";
 
@@ -55,6 +83,8 @@ internal class DeckbuilderViewModel : INotifyPropertyChanged
             OnPropertyChanged(nameof(WindowTitle));
         }
     }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     protected virtual void OnPropertyChanged(string propertyName)
     {
