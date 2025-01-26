85baebf0ee1c2410d648cbc4593fee555c87d625
Ere <animeftw@live.co.uk>
2025-01-27 07:33:09 +1100

    Failed attempt at using dependency injection in content view

--- src/Decksteria.Ui.Maui/Pages/Deckbuilder/Deckbuilder.xaml.cs
+++ src/Decksteria.Ui.Maui/Pages/Deckbuilder/Deckbuilder.xaml.cs
@@ -14,8 +14,10 @@ using Decksteria.Services.DeckFileService.Models;
 using Decksteria.Ui.Maui.Pages.CardInfo;
 using Decksteria.Ui.Maui.Pages.DeckStatistics;
 using Decksteria.Ui.Maui.Pages.Search;
+using Decksteria.Ui.Maui.Services.CardImageService;
 using Decksteria.Ui.Maui.Services.DeckFileService;
 using Decksteria.Ui.Maui.Services.PageService;
+using Decksteria.Ui.Maui.Shared.Controls;
 using Decksteria.Ui.Maui.Shared.Extensions;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
@@ -36,6 +38,8 @@ public partial class Deckbuilder : UraniumContentPage
 
     private readonly IPageService pageService;
 
+    private readonly IDecksteriaCardImageService cardImageService;
+
     private IDeckbuildingService deckbuilder;
 
     private SearchModal? searchModal = null;
@@ -46,7 +50,7 @@ public partial class Deckbuilder : UraniumContentPage
 
     private bool firstLoaded = false;
 
-    public Deckbuilder(IDeckbuildingServiceFactory deckbuilderFactory, IDeckFileServiceFactory deckFileServiceFactory, IPageService pageService)
+    public Deckbuilder(IDeckbuildingServiceFactory deckbuilderFactory, IDeckFileServiceFactory deckFileServiceFactory, IPageService pageService, IDecksteriaCardImageService cardImageService)
     {
         InitializeComponent();
         BindingContext = viewModel;
@@ -54,6 +58,7 @@ public partial class Deckbuilder : UraniumContentPage
         this.deckbuilder = deckbuilderFactory.GetCurrentDeckbuildingService();
         this.deckFileService = deckFileServiceFactory.GetDeckFileService();
         this.pageService = pageService;
+        this.cardImageService = cardImageService;
         this.searchFieldFilters = Array.Empty<ISearchFieldFilter>();
     }
 
@@ -429,5 +434,15 @@ public partial class Deckbuilder : UraniumContentPage
         collection.UpdateData(newData);
         collectionView.ItemsSource = collection;
         viewModel.ValidDeckStatus = await deckbuilder.ValidDecklistAsync(cancellationToken);
+    }
+
+    private void DecksteriaImageControl_Loaded(object sender, EventArgs e)
+    {
+        if (sender is not DownloadableImage imageControl)
+        {
+            return;
+        }
+
+        imageControl.SetCardImageService(cardImageService);
     }
 }
